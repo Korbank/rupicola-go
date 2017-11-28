12bdee180de0c28ff3f37a4aac71d67c3e1b6772
Kociołek <kociolek.farfocle@gmail.com>
2017-11-28 20:54:45 +0100

    Fix build on unix

--- utils_nix.go
+++ utils_nix.go
@@ -8,8 +8,9 @@ import (
 	"syscall"
 )
 
+// SetUserGroup assign UID and GID to process
 func SetUserGroup(process *exec.Cmd, m *MethodDef) {
 	log.Println("Nix code")
 	process.SysProcAttr = &syscall.SysProcAttr{}
-	process.SysProcAttr.Credential = &syscall.Credential{Uid: *m.InvokeInfo.RunAs.Uid, Gid: *m.InvokeInfo.RunAs.Gid}
+	process.SysProcAttr.Credential = &syscall.Credential{Uid: *m.InvokeInfo.RunAs.UID, Gid: *m.InvokeInfo.RunAs.GID}
 }
