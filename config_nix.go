ac3e4478aba611bbfd43f92dc9f1831cafb522d4
Marcin Kocot <me@foo.bz>
2018-05-03 00:33:20 +0200

    Fix linux build

--- config_nix.go
+++ config_nix.go
@@ -5,6 +5,6 @@ package main
 import "log/syslog"
 import log "github.com/inconshreveable/log15"
 
-func configureSyslog(path string) (string, error) {
+func configureSyslog(path string) (log.Handler, error) {
 	return log.SyslogNetHandler("", path, syslog.LOG_DAEMON, "rupicola", log.JsonFormat())
 }
