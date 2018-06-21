0fe8f92d4c1b9f880507705464d7b2c6f3067452
Marcin <kociolek.farfocle@gmail.com>
2018-06-21 23:08:42 +0200

    Stack dosn't need to be public

--- internal/pkg/config/stack.go
+++ internal/pkg/config/stack.go
@@ -17,18 +17,18 @@ func (q *Queue) Len() int {
 	return len(*q)
 }
 */
-type Stack []string
+type stack []string
 
-func (q *Stack) Push(n string) {
+func (q *stack) push(n string) {
 	*q = append(*q, n)
 }
 
-func (q *Stack) Pop() (n string) {
-	x := q.Len() - 1
+func (q *stack) pop() (n string) {
+	x := q.len() - 1
 	n = (*q)[x]
 	*q = (*q)[:x]
 	return
 }
-func (q *Stack) Len() int {
+func (q *stack) len() int {
 	return len(*q)
 }
